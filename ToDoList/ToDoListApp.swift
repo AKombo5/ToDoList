11a23623cf5b45297c82bd4d1d1f0c7a6112838d
Andrew Kombouras <kombouan@bc.edu>
2022-10-17 01:44:54 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Andrew Kombouras on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
